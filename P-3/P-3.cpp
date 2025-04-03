8f3907cacce4f9999c1b8ff8fc123efab9f680bd
SethS05 <s-skinner@onu.edu>
2025-04-03 00:46:02 -0400

    Initialized Visual Studio Project

---
+++ P-3/P-3.cpp
@@ -0,0 +1,17 @@
+/*
+* Author: Seth Skinner
+* File: P9-3
+* Date: 4/3/2025
+* 
+* Description:program that generates a sequence of 20 random six-sided die tosses, determines whether or not a run 
+* is present and prints out this information, and prints all the die values, marking the runs by including them in parentheses
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	 
+}
