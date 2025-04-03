d8b400037c7b2ab077f23468546e867286a92588
SethS05 <s-skinner@onu.edu>
2025-04-03 00:48:28 -0400

    Accident

--- P-3/P-3.cpp
+++ P-3/P-3.cpp
@@ -8,10 +8,46 @@
 */
 
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
 
 using namespace std;
 
+void displayRun(int values[], int SIZE);
+
 int main()
 {
-	 
+    const int SIZE = 20;
+    int values[SIZE];
+
+    // Seed the random number generator
+    srand(time(0));
+
+    // Generate random values for the array
+    for (int i = 0; i < SIZE; i++) {
+        values[i] = rand() % 6 + 1;
+    }
+
+    // Display the sequence and check for runs
+    cout << "Die values: ";
+    for (int i = 0; i < SIZE; ++i) {
+        cout << values[i] << " ";
+    }
+    cout << endl;
+
+    displayRun(values, SIZE);
+
+    return 0;
+}
+
+void displayRun(int values[], int SIZE) {
+    for (int i = 0; i < SIZE; i++) {
+        if (values[i] == values[i + 1] && values[i] != values[i - 1]) {
+            cout << " (";
+        }
+        cout << " " << values[i];
+        if (values[i] != values[i + 1] && values[i] == values[i - 1]) {
+            cout << ") ";
+        }
+    }
 }
