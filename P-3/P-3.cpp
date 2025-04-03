060a693c5abe107dd090eaf1e06704b9e2f08e7c
SethS05 <s-skinner@onu.edu>
2025-04-03 00:50:38 -0400

    completed displayRun

--- P-3/P-3.cpp
+++ P-3/P-3.cpp
@@ -17,27 +17,7 @@ void displayRun(int values[], int SIZE);
 
 int main()
 {
-    const int SIZE = 20;
-    int values[SIZE];
-
-    // Seed the random number generator
-    srand(time(0));
-
-    // Generate random values for the array
-    for (int i = 0; i < SIZE; i++) {
-        values[i] = rand() % 6 + 1;
-    }
-
-    // Display the sequence and check for runs
-    cout << "Die values: ";
-    for (int i = 0; i < SIZE; ++i) {
-        cout << values[i] << " ";
-    }
-    cout << endl;
-
-    displayRun(values, SIZE);
-
-    return 0;
+   
 }
 
 void displayRun(int values[], int SIZE) {
